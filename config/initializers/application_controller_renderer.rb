1e7889a70c0efd10039a0b153771d448e1bf679d
Misa-Kanuma <misa.kanuma@gmail.com>
2020-06-11 16:16:29 +0900

    add sample

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'e3282fcce6a4eef344f2a22030a98bfa708fbaf459da4bb3a518fc7770a3485ff4b7d69859597ab23d5765e770f13026d9ba4079b2c68920f41e9d7d3f864689'
